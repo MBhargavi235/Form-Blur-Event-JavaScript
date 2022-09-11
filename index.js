39a023854619600d65c3d1fc94d59cd54b4de4d2
MBhargavi235 <106804698+MBhargavi235@users.noreply.github.com>
2022-09-11 19:31:36 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+let myFormEl = document.getElementById("myForm");
+let nameEl = document.getElementById("name");
+let nameErrMsgEl = document.getElementById("nameErrMsg");
+
+let emailEl = document.getElementById("email");
+let emailErrMsgEl = document.getElementById("emailErrMsg");
+
+nameEl.addEventListener("blur", function(event) {
+  if (event.target.value === "") {
+    nameErrMsgEl.textContent = "Required*";
+  } else {
+    nameErrMsgEl.textContent = "";
+  }
+});
+
+emailEl.addEventListener("blur", function(event) {
+  if (event.target.value === "") {
+    emailErrMsgEl.textContent = "Required*";
+  } else {
+    emailErrMsgEl.textContent = "";
+  }
+});
+
+myFormEl.addEventListener("submit", function(event) {
+  event.preventDefault();
+  document.querySelector(".user-details").innerHTML=
+   `
+  <p>${nameEl.value}</p>
+  <p>${emailEl.value}</p>
+  `;
+  nameEl.value="";
+  emailEl.value="";
+});
